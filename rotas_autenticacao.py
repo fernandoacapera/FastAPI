8b9d65d66150d2e6840c3abd49ab06f216470553
Fernando <fernandocapera2018@outlook.com>
2025-11-13 17:43:59 -0300

    FastAPI 13/11/2025

--- rotas_autenticacao.py
+++ rotas_autenticacao.py
@@ -8,3 +8,4 @@ async def autenticar():
     Essa é a rota padrão de autenticação do nosso sistema
     """
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado":False}
+
